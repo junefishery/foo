918407a41f40f97649843e9fca261a25c33c44bd
junefishery <124114889+junefishery@users.noreply.github.com>
2023-02-01 20:02:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,11 +10,11 @@ if url:
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     paragraphs = soup.find_all("p", {"class": "pw-post-body-paragraph"})
-    article_content = "\n".join([p.get_text() for p in paragraphs])
-
-    # Create a text area widget
-    text_area = st.text_area("Article content", article_content)
-    
-    # Add a select all button
-    if st.button("Select all"):
-        text_area.text = article_content
+    article_content = "\n\n".join([p.get_text() for p in paragraphs])
+    text_area = st.text_area(article_content, key='text_area')
+    if st.button("Select text"):
+        st.write("""
+        <script>
+            document.getElementById("text_area-0").select();
+        </script>
+        """, unsafe_allow_html=True)
