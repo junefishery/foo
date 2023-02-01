28de7305ef460be34e4118b21aaf9cf0656685d9
junefishery <124114889+junefishery@users.noreply.github.com>
2023-02-01 20:09:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,7 @@ if url:
     soup = BeautifulSoup(page.content, 'html.parser')
     paragraphs = soup.find_all("p", {"class": "pw-post-body-paragraph"})
     article_content = "\n\n".join([p.get_text() for p in paragraphs])
-    text_area = st.text_area(article_content, key='text_area')
+    st.write("Article content:", unsafe_allow_html=True)
+    text_area = st.text_area(article_content)
     if st.button("Select text"):
-        st.write("""
-        <script>
-            document.getElementById("text_area-0").select();
-        </script>
-        """, unsafe_allow_html=True)
+        text_area.select()
