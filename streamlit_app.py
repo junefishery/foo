8c0dae8c27dde299b87b92dcdf7afce28e949328
junefishery <124114889+junefishery@users.noreply.github.com>
2023-02-01 18:58:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,25 +2,12 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 
-def scrape_wikipedia():
-    # Make a GET request to the Wikipedia home page
-    res = requests.get('https://en.wikipedia.org/wiki/Special:Random')
+st.title("Medium article display")
 
-    # Parse the HTML with BeautifulSoup
-    soup = BeautifulSoup(res.text, 'html.parser')
+url = st.text_input("Enter the URL of the Medium article:")
 
-    # Extract the title and content of the article
-    title = soup.select('h1')[0].text
-    content = soup.select('div.mw-parser-output')[0].text
-
-    return title, content
-
-title, content = scrape_wikipedia()
-
-st.title("Random Wikipedia Article")
-if st.button("Refresh"):
-    title, content = scrape_wikipedia()
-
-st.write("")
-st.write("### Title:", title, "")
-st.write("Content:", content)
+if url:
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    article_content = soup.find("div", {"class": "section-inner"}).get_text()
+    st.write(article_content)
