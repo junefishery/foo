4061e78ca988f532748bb2a9b2a21009e3ccdc7b
junefishery <124114889+junefishery@users.noreply.github.com>
2023-02-01 20:20:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,5 @@ if url:
     soup = BeautifulSoup(page.content, 'html.parser')
     paragraphs = soup.find_all("p", {"class": "pw-post-body-paragraph"})
     article_content = "\n\n".join([p.get_text() for p in paragraphs])
-    st.write("Article content:", unsafe_allow_html=True)
+#     st.write("Article content:", unsafe_allow_html=True)
     st.text_area(article_content, height=500)
