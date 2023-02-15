2d04627a0e961fb2aacde819e04d303815037a32
junefishery <124114889+junefishery@users.noreply.github.com>
2023-02-15 19:17:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,30 @@
+# Import streamlit library
 import streamlit as st
-import requests
-from bs4 import BeautifulSoup
 
-st.title("Medium article display")
+# Import urllib library
+import urllib
 
-url = st.text_input("Enter the URL of the Medium article:")
+# Create a text input for the user to enter a list of words
+words = st.text_input("Enter a list of words separated by commas", value="long,gift,master")
 
-if url:
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    paragraphs = soup.find_all("p", {"class": "pw-post-body-paragraph"})
-    article_content = "\n\n".join([p.get_text() for p in paragraphs])
-    st.text_area("Content:", value = article_content, height=500)
+# Check if the user has entered any words
+if words:
+    # Split the words by commas and strip any whitespace
+    words = [word.strip() for word in words.split(",")]
+
+    # Create a Bing query in the desired format
+    query = f"write a short story with these words involved: {', '.join(words)}"
+    query_origin = query
+
+    # Replace all spaces with "+" symbols
+    # query = query.replace(" ", "+")
+
+    # Encode the query for the URL
+    query = urllib.parse.quote(query)
+
+    # Create a Bing URL with the query
+    url = f"https://www.bing.com/search?q={query}"
+
+    # Display the query as a link
+    st.write(f"**Your Bing query is:** {query_origin}")
+    st.markdown(f'<a href="{url}" target="_blank"><button>Let Bing AI create a story</button></a>', unsafe_allow_html=True)
